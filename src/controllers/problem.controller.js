add88fc1e69bc91616c8d70078b31fe82f1e0e09
Sanket Singh <singhsanket143@gmail.com>
2024-03-19 22:29:33 +0530

    Feat: added unimplemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -3,23 +3,33 @@ function pingProblemController(req, res) {
 }
 
 function addProblem(req, res) {
-
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 function getProblem(req, res) {
-
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 function getProblems(req, res) {
-
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 function deleteProblem(req, res) {
-
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 function updateProblem(req, res) {
-
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 module.exports = {
