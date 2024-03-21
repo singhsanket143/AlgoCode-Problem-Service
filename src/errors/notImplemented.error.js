e753dd5e591852466662ad54cd19d5f724f32543
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-03-21 23:48:03 +0530

    Feature_Add_more_errors

--- src/errors/notImplemented.error.js
+++ src/errors/notImplemented.error.js
@@ -1,10 +1,15 @@
-const BaseError = require('./base.error');
-const { StatusCodes } = require('http-status-codes');
+const BaseError = require("./base.error");
+const { StatusCodes } = require("http-status-codes");
 
 class NotImplemented extends BaseError {
-    constructor(methodName) {
-        super("NotImplemented", StatusCodes.NOT_IMPLEMENTED, `${methodName} Not Implemented`, {});
-    }
+  constructor(methodName) {
+    super(
+      "Not Implemented",
+      StatusCodes.NOT_IMPLEMENTED,
+      `Not Implemented Error: ${methodName} Not Implemented`,
+      {}
+    );
+  }
 }
 
 module.exports = NotImplemented;
