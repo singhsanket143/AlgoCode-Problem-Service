0750efe9ae162413e7f46890c9eeba95401321f4
Rohit Shah <shahrohit2061@gmail.com>
2024-03-22 14:37:38 +0530

    Fix : next middleware in errorHandler and problem Controller and error messages

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,51 +1,50 @@
 const { StatusCodes } = require('http-status-codes');
-const NotImplemented = require('../errors/notImplemented.error');
-const BadRequest = require('../errors/badrequest.error');
+const { NotImplementedError } = require('../errors/index.error');
 
 function pingProblemController(req, res) {
-    return res.json({message: 'Problem controller is up'});
+    return res.status(StatusCodes.OK).json({message: 'Problem controller is up'});
 }
 
 function addProblem(req, res, next) {
     try {
         // nothing implemented
-        throw new NotImplemented('Add Problem');
+        throw new NotImplementedError('Add Problem');
     } catch(error) {
         next(error);
     }
 }
 
-function getProblem(req, res) {
+function getProblem(req, res, next) {
     try {
         // nothing implemented
-        throw new NotImplemented('Add Problem');
+        throw new NotImplementedError('Get Problem');
     } catch(error) {
         next(error);
     }
 }
 
-function getProblems(req, res) {
+function getProblems(req, res, next) {
     try {
         // nothing implemented
-        throw new NotImplemented('Add Problem');
+        throw new NotImplementedError('Get Problems');
     } catch(error) {
         next(error);
     }
 }
 
-function deleteProblem(req, res) {
+function deleteProblem(req, res, next) {
     try {
         // nothing implemented
-        throw new NotImplemented('Add Problem');
+        throw new NotImplementedError('Delete Problem');
     } catch(error) {
         next(error);
     }
 }
 
-function updateProblem(req, res) {
+function updateProblem(req, res, next) {
     try {
         // nothing implemented
-        throw new NotImplemented('Add Problem');
+        throw new NotImplementedError('Update Problem');
     } catch(error) {
         next(error);
     }
