55936446857434e9f46e8b467d026c9bcffd3af1
vivektarun <vivektarun1234@gmail.com>
2024-04-03 01:44:44 +0530

    feat: implemented updateProblem  function

--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -42,6 +42,19 @@ class ProblemRepository {
         }
     }
 
+    async updateProblem(id) {
+        try {
+            const updatedProblem = await Problem.findByIdAndUpdate(id, updatedData, { new: true });
+            if (!updatedProblem) {
+                throw new NotFound("Problem", id);
+            }
+            return updatedProblem;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
 }
 
 module.exports = ProblemRepository;
