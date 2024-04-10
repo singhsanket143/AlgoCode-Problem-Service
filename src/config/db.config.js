d0fd894f96637a0d431cbc90f99edd5d5ce7a27b
vivektarun <vivektarun1234@gmail.com>
2024-04-10 15:13:14 +0530

    fix: console of successfully connected to database

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -7,6 +7,7 @@ async function connectToDB() {
     try {
         if(NODE_ENV == "development") {
             await mongoose.connect(ATLAS_DB_URL);
+            console.log('successfully connected to database');
         } 
     } catch(error) {
         console.log('Unable to connect to the DB server');
--- src/index.js
+++ src/index.js
@@ -28,5 +28,4 @@ app.use(errorHandler);
 app.listen(PORT, async () => {
     console.log(`Server started at PORT: ${PORT}`);
     await connectToDB();
-    console.log("Successfully connected to db");
 });
